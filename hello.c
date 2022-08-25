a9fb51d309425b2aaceacbfd43520136436cf915
jonathan czepa <jonathan.czepa@gmail.com>
2022-08-25 21:54:28 +0000

    added name

--- hello.c
+++ hello.c
@@ -14,5 +14,7 @@ int main(int argc, char **argv) {
 	printf("Hello World!\n");
 	printf("Johnathan Riggins!\n");
 	printf("Computer Engineering!\n");
+	printf("Jonathan Czepa Here!\n");
+	printf("Also Computer Engineering!\n");
 	return 0;
 }
