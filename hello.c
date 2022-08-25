f24f2afaf73722e867a09e78014f5ceac87bda94
Johnathan Riggins <jriggins3@huskers.unl.edu>
2022-08-25 15:10:46 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Johnathan Riggins
+ *         jriggins3@huskers.unl.edu
+ * Date: 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+	printf("Hello World!\n");
+	printf("Johnathan Riggins\n");
+	printf("Jonathan Czepa");
+
+	return 0;
+}
