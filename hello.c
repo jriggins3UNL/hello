f48d8696b8f678bf2be469155347a73c8fab5fe8
Johnathan Riggins <jriggins3@huskers.unl.edu>
2023-02-03 09:56:08 -0600

    Initial Commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Johnathan Riggins
+ * jriggins.unomaha.edu
+ *
+ * 2023/02/03
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main( int argc, char **argv ) {
+	printf("Johnathan\n");
+
+	return 0;
+}
